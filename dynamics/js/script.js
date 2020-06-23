69bd9a2e3a386a0f2e276ae1345832971a9add37
Naimad Gam <raul3e@comunidad.unam.mx>
2020-06-22 19:26:17 -0500

    FEAT:Agregada pantalla de inicio

---
+++ dynamics/js/script.js
@@ -0,0 +1,12 @@
+let encuesta= document.getElementById("button");
+encuesta.addEventListener("click", ()=>{
+  window.location = "./templates/encuesta.html";
+});
+let inicio= document.getElementById("iniciar");
+inicio.addEventListener("click", ()=>{
+  window.location = "./templates/encuesta.html";
+});
+let registro= document.getElementById("registro");
+registro.addEventListener("click", ()=>{
+  window.location = "./templates/encuesta.html";
+});
